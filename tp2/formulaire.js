80569d6ef1053f994842b51cb860330458fac4bf
djennadi-liticia <115448950+djennadi-liticia@users.noreply.github.com>
2022-10-25 18:16:06 +0200

    TP2 -Finalisé-

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -4,28 +4,36 @@ function validation(){
     d=document.getElementById("date").value;
     a=document.getElementById("adresse").value;
     m=document.getElementById("mail").value;
+    document.getElementById("res").style.display ="none";
+    document.getElementById("error").style.display ="none";
     if(n.length<5 || n==""){
-    document.getElementById("error").innerHTML = "La saisie du nom est obligatoire";
+        document.getElementById("error").style.display ="block";
+    document.getElementById("error").innerHTML = "La saisie du nom est obligatoire avec une taille min de 5 caractères";
     return false;
     }
     else if(m.length<5 || m==""){
-        document.getElementById("error").innerHTML = "La saisie du mail est obligatoire";
+        document.getElementById("error").style.display ="block";
+        document.getElementById("error").innerHTML = "La saisie du mail est obligatoire avec une taille min de 5 caractères";
         return false;
         }
      else if(p.length<5 || p==""){
-            document.getElementById("error").innerHTML = "La saisie du prenom est obligatoire";
+        document.getElementById("error").style.display ="block";
+            document.getElementById("error").innerHTML = "La saisie du prenom est obligatoire avec une taille min de 5 caractères";
             return false;
         }
         else if(d.length<5 || d==""){
-            document.getElementById("error").innerHTML = "La saisie de la date est obligatoire";
+            document.getElementById("error").style.display ="block";
+            document.getElementById("error").innerHTML = "La saisie de la date est obligatoire avec une taille min de 5 caractères ";
             return false;
             }
             else if(a.length<5 || a==""){
-                document.getElementById("error").innerHTML = "La saisie de l'adresse est obligatoire";
+                document.getElementById("error").style.display ="block";
+                document.getElementById("error").innerHTML = "La saisie de l'adresse est obligatoir eavec une taille min de 5 caractères ";
                 return false;
                 }
                 else{
-                document.getElementById("res").innerHTML = "Bienvenue " +n;
+                    document.getElementById("res").style.display ="block";
+                document.getElementById("res").innerHTML= "Bienvenue " +n;
                 return true;
                 }
 }
