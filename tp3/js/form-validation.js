56cd93df8a58874a445c4d7df8cbf36a57e9142f
djennadi-liticia <115448950+djennadi-liticia@users.noreply.github.com>
2022-11-19 22:03:58 +0100

    TP3

--- tp3/js/form-validation.js
+++ tp3/js/form-validation.js
@@ -1,47 +1,32 @@
-function Validation(){
-window.onload = function () {   
-     console.log( "DOM ready!" );
-     
-     n=document.getElementById("nom").value;
+function validateEmail(email) {
+   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   return re.test(String(email).toLowerCase());
+ } 
+function testdate() {  
+let dateNaissance = new Date(2022, 10, 25); 
+let dateNaissanceTimestamp = dateNaissance.getTime();
+let nowTimestamp = Date.now();
+if(nowTimestamp>d){ 
+   return false;
+}else{
+   return true;
+}
+}
+function Validation(){ 
+    n=document.getElementById("nom").value;
     p=document.getElementById("prenom").value;
     d=document.getElementById("date").value;
-    a=document.getElementById("adresse").value;
     m=document.getElementById("mail").value;
-       function validateEmail(email) {
-         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-         return re.test(String(email).toLowerCase());
-       }
-         let dateNaissance = new Date(2022, 10, 25); 
-         let dateNaissanceTimestamp = dateNaissance.getTime();
-         let nowTimestamp = Date.now();
-         var myModal = new bootstrap.Modal(document.getElementById('myModal'));
-         myModal.show();
-         if(n.length>5 && p.length>5 && d.length >5 && d>nowTimestamp && m>5 && validateEmail(m)){
-          var map;
-          function initialize() {
-            var prop = {
-             center:new google.maps.LatLng(51.508742,-0.120850),
-             zoom:5,
-             mapTypeId:google.maps.MapTypeId.ROADMAP
-            };
-           return new google.maps.Map(document.getElementById("w3docs-map"), prop);
-          }
-          setTimeout(function(){
-             map = initialize();
-          },500);
-          function w3Load(){
-             document.getElementById("w3docs-map").style.display = 'block';
-             google.maps.event.trigger(map, 'resize');
-          }
-          document.querySelector(".modal-body").innerHTML = '\<img src="map.jpg"/\>'
-          document.querySelector(".modal-title").textContent = "Bienvenue" +n;
-
+    var myModal = new bootstrap.Modal(document.getElementById('myModal'));       
+   if(n.length>5 && p.length>5 && testdate()==true && validateEmail(m)==true){
+       adr=document.getElementById("adresse").value;
+          document.querySelector(".modal-body").innerHTML = `Vous etes nés le ${d} et Vous habitez à : <a href='http://maps.google.com/maps?q=${adr}'><img src='https://maps.googleapis.com/maps/api/staticmap?markers=${adr}&zoom=14&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg' width='100%' />${adr}</a>`;
+          document.querySelector(".modal-title").textContent = "Bienvenue " +n;
+          myModal.show();
          }
-    else{
+   else{
       document.querySelector(".modal-title").textContent = "Erreur dans le formulaire";
       document.querySelector(".modal-body").innerHTML= "Tout les champs sont obligatoires" ;
-    }
-    
+      myModal.show();
+   }
   }
-    
-    };
