5caaa8563c2d7a97521c0a1635de236a5aed4708
djennadi-liticia <115448950+djennadi-liticia@users.noreply.github.com>
2023-01-23 16:00:20 +0100

    TP6

--- tp5/js/main.js
+++ tp5/js/main.js
@@ -103,7 +103,5 @@ window.onload = function () {
         });        
     }
 }
-
     });
-
 }
--- tp6/meteo-angular/src/app/app.module.ts
+++ tp6/meteo-angular/src/app/app.module.ts
@@ -4,7 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeteoComponent } from './meteo/meteo.component';
-
+const appRoutes: Routes = [
+  {
+    path: '', // la page principale utilisera le component suivant
+    component: MeteoComponent,
+  },
+  {
+    path: 'meteo/:name', // la page affichant la météo prendra comme paramètre 'name'
+    component: MeteoDetailComponent, // Ce component fera l'appel AJAX et afficher les données reçues par openWeatherMap
+  },
+  {
+    path: '**', // un chemin vers une page inexistante redirigera vers '/'
+    redirectTo: '/',
+    pathMatch: 'full',
+  },
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +27,12 @@ import { MeteoComponent } from './meteo/meteo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     
   ],
   providers: [],
@@ -23,3 +42,5 @@ import { MeteoComponent } from './meteo/meteo.component';
 export class AppModule { }
 import { RouterModule, Routes } from '@angular/router';
 import { MeteoDetailComponent } from './meteo-detail/meteo-detail.component';
+import { FormsModule } from '@angular/forms';
+
